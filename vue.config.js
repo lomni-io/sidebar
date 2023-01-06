c49ce78f71a9fc4b563d5272f7b245072670d1e7
Daniel Kanczuk <danielsussa@gmail.com>
2023-01-06 10:56:41 -0300

    Rc/0.8.1 (#2)

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/sidebar/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/TO_REPLACE_HERE/' : '/',
   devServer: {
     allowedHosts: 'all',
   },
